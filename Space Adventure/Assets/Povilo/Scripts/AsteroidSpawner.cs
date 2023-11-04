4d7a486aa13caa60b78c06787a1665507e50aefc
PovilasDuo <duobapovilas@gmail.com>
2023-11-04 18:30:00 +0200

    Added countdown
    Added intro countdown and UI

--- Space Adventure/Assets/Povilo/Scripts/AsteroidSpawner.cs
+++ Space Adventure/Assets/Povilo/Scripts/AsteroidSpawner.cs
@@ -20,6 +20,8 @@ public class AsteroidSpawner : MonoBehaviour
 	public float asteroidSpeed = 750;
 	public float asteroidRotationSpeed = 2f;
 
+	public bool gameStart = false;
+
 	// Start is called before the first frame update
 	void Start()
 	{
@@ -43,52 +45,56 @@ public class AsteroidSpawner : MonoBehaviour
 	/// <returns></returns>
 	IEnumerator SpawnAsteroids()
 	{
-		float cameraHeight = 2f * mainCamera.orthographicSize;
-		float cameraWidth = cameraHeight * mainCamera.aspect;
-		float spawnX, spawnY;
-		int spawnType = Random.Range(0, 4);
-		switch (spawnType)
+		if (gameStart)
 		{
-			case 0:
-				spawnX = cameraWidth;
-				spawnY = Random.Range(-cameraHeight, cameraHeight);
-				break;
-			case 1:
-				spawnX = -cameraWidth;
-				spawnY = Random.Range(-cameraHeight, cameraHeight);
-				break;
-			case 2:
-				spawnX = Random.Range(-cameraWidth, cameraWidth);
-				spawnY = cameraHeight;
-				break;
-			default:
-				spawnX = Random.Range(-cameraWidth, cameraWidth);
-				spawnY = -cameraHeight;
-				break;
-		}
-		Vector3 spawnPosition = new(spawnX, spawnY, 0f);
 
-		int spawnScaleRNG = Random.Range(0, 100);
-		int spawnScale;
-		switch (spawnScaleRNG)
-		{
-			case > 90:
-				spawnScale = 3;
-				break;
-			case > 60:
-				spawnScale = 2;
-				break;
-			default:
-				spawnScale = 1;
-				break;
-		}
-		asteroidPrefab = asteroidList[Random.Range(0, asteroidList.Count - 1)];
-		GameObject asteroidSpawned = Instantiate(asteroidPrefab, spawnPosition, Quaternion.identity);
-		asteroidSpawned.transform.localScale *= spawnScale;
+			float cameraHeight = 2f * mainCamera.orthographicSize;
+			float cameraWidth = cameraHeight * mainCamera.aspect;
+			float spawnX, spawnY;
+			int spawnType = Random.Range(0, 4);
+			switch (spawnType)
+			{
+				case 0:
+					spawnX = cameraWidth;
+					spawnY = Random.Range(-cameraHeight, cameraHeight);
+					break;
+				case 1:
+					spawnX = -cameraWidth;
+					spawnY = Random.Range(-cameraHeight, cameraHeight);
+					break;
+				case 2:
+					spawnX = Random.Range(-cameraWidth, cameraWidth);
+					spawnY = cameraHeight;
+					break;
+				default:
+					spawnX = Random.Range(-cameraWidth, cameraWidth);
+					spawnY = -cameraHeight;
+					break;
+			}
+			Vector3 spawnPosition = new(spawnX, spawnY, 0f);
 
-		Vector3 direction = (Camera.main.transform.position - asteroidSpawned.GetComponent<Transform>().position).normalized;
-		asteroidSpawned.GetComponent<Rigidbody>().AddForce(direction * asteroidSpeed);
-		asteroidSpawned.GetComponent<Rigidbody>().angularVelocity = direction * asteroidRotationSpeed;
-		yield return new WaitForSeconds(spawnSpeed);
+			int spawnScaleRNG = Random.Range(0, 100);
+			int spawnScale;
+			switch (spawnScaleRNG)
+			{
+				case > 90:
+					spawnScale = 3;
+					break;
+				case > 60:
+					spawnScale = 2;
+					break;
+				default:
+					spawnScale = 1;
+					break;
+			}
+			asteroidPrefab = asteroidList[Random.Range(0, asteroidList.Count - 1)];
+			GameObject asteroidSpawned = Instantiate(asteroidPrefab, spawnPosition, Quaternion.identity);
+			asteroidSpawned.transform.localScale *= spawnScale;
+
+			Vector3 direction = (Camera.main.transform.position - asteroidSpawned.GetComponent<Transform>().position).normalized;
+			asteroidSpawned.GetComponent<Rigidbody>().AddForce(direction * asteroidSpeed);
+			asteroidSpawned.GetComponent<Rigidbody>().angularVelocity = direction * asteroidRotationSpeed;
+			yield return new WaitForSeconds(spawnSpeed);
+		}
 	}
 }
